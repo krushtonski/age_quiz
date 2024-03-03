56315a56da8825e3c3c3e09cf3645cfd51679b49
Kate <krushtie33@gmail.com>
2024-03-03 17:48:11 +0000

    changed age variable to age_input and user_age for clarity age_quiz.py

--- age_quiz.py
+++ age_quiz.py
@@ -1,17 +1,20 @@
 # request user input their age as a whole number and store as variable "age"
-age = input("Please enter your age as a whole number between 1 and 100:")
+age_input = input("Please enter your age as a whole number between 1 and 100:")
 # convert age input to integer
-age = int(age)
-# create conditionals starting with the equal to 21 conditional, then the age conditionals starting with the highest age number conditional and descending in order
-if age == 21:
+user_age = int(age_input)
+
+# create conditionals starting with the equal to 21 conditional, then the 
+# age conditionals starting with the highest age number conditional and descending in order
+
+if user_age == 21:
     print("Congrats on your 21st!")
-elif age > 100:
+elif user_age > 100:
     print("Sorry, you're dead.")
-elif age >= 65:
+elif user_age >= 65:
     print("Enjoy your retirement!")
-elif age >= 40:
+elif user_age >= 40:
     print("You're over the hill.")
-elif age < 13:
+elif user_age < 13:
     print("You qualify for the kiddie discount.")
 # Create an else conditional for all the other age input values
 else:
