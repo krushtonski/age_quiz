1410c5f96785066ef394a104f5d195439bb29669
Kate <krushtie33@gmail.com>
2024-03-03 18:03:54 +0000

    added better and clearer description for age conditionals age_quiz.py

--- age_quiz.py
+++ age_quiz.py
@@ -6,16 +6,16 @@ user_age = int(age_input)
 # create conditionals starting with the equal to 21 conditional, then the 
 # age conditionals starting with the highest age number conditional and descending in order
 
-if user_age == 21:
+if user_age == 21: #for users aged 21
     print("Congrats on your 21st!")
-elif user_age > 100:
+elif user_age > 100: #for users older than 100
     print("Sorry, you're dead.")
-elif user_age >= 65:
+elif user_age >= 65: #for users 65 years old and older
     print("Enjoy your retirement!")
-elif user_age >= 40:
+elif user_age >= 40: #for users 40 years old and older
     print("You're over the hill.")
-elif user_age < 13:
+elif user_age < 13: #for users under 13
     print("You qualify for the kiddie discount.")
-# Create an else conditional for all the other age input values
-else:
-    print("Age is but a number.")
+
+else: #for user who are in another age category than the ones above
+    print("Age is but a number.") 
